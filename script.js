d48fbae7e1adf44e7ca2cc54cb866cd266c24107
Jenifer Scarlate <jenifer.scarlate@unesp.br>
2024-04-15 09:03:49 -0300

    script: alterando o nome de algumas variáveis

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function CapturarValores() {
 
     const dadosUsuario = {
         nome: nome,
-        height: altura,
+        altura: altura,
         peso: peso
     }
     return dadosUsuario;
@@ -31,14 +31,20 @@ function ClassificarImc(imc) {
     if(imc < 18.5) {
         return 'Abaixo do peso!';
     }
-    else if(imc < 25) {
+    else if (imc >= 18.5 && imc < 24.9) {
         return 'Peso normal';
     }
-    else if(imc < 30) {
+    else if (imc >= 25.0 && imc < 29.9) {
         return 'Sobrepeso';
     }
+    else if (imc >= 30.0 && imc < 34.9) {
+        return 'Obesidade grau I';
+    }
+    else if (imc >= 35.0 && imc < 39.9) {
+        return 'Obesidade grau II';
+    }
     else{
-        return 'Obesidade';
+        return 'Obesidade Grau III (Morbida)';
     }
 }
 
