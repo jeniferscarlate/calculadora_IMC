90f11c3d9bdccea006eb2380b6edd1adf9b35625
jenifer scarlate <jenifer.scarlate@unesp.br>
2024-04-16 16:41:59 -0300

    script: terminando as funções de limpar registros, calcular valores, carregar usuarios e montar tabela

--- script.js
+++ script.js
@@ -4,10 +4,30 @@
 //2. calcular IMC - ok
 //3. gerar classificação do IMC - ok
 //4. organizar as informações - ok
-//5. salvar os dados na lista 
-//6. ler a lista com os dados
+//5. salvar os dados na lista - ok
+//6. ler a lista com os dados -
 //7. renderizar o conteúdo no HTML (tabela)
 //8. botão de limpar os registros (clear (LocalStorage))
+
+
+ //responsavel por chamar todas as outras funções
+function CalcularValores(event) {
+    event.preventDefault();
+
+    let dadosUsuario = CapturarValores();
+
+    let imc = CalcularImc(dadosUsuario.altura, dadosUsuario.peso);
+
+    let classificacao = ClassificarImc(imc);
+
+    let dadosUsuarioCompleto = OrganizarDados(dadosUsuario, imc, classificacao);
+
+    CadastrarUsuario(dadosUsuarioCompleto);
+
+    window.location.reload();
+
+}
+
 
 function CapturarValores() {
     const nome = document.getElementById('name').value;
@@ -22,7 +42,7 @@ function CapturarValores() {
     return dadosUsuario;
 }
 
-function CalcularImc(peso, altura) {
+function CalcularImc(altura, peso) {
     const imc = peso / (altura * altura);
     return imc;
 }
@@ -49,7 +69,9 @@ function ClassificarImc(imc) {
 }
 
 function OrganizarDados(dadosUsuario, valorImc, classificacaoImc) {
-    const dataHoraAtual = Intl.DateTimeFormat('pt-BR', {timeStyle: 'long', dateStyle: "short"}).format(Date.now());
+
+    const dataHoraAtual = Intl.DateTimeFormat('pt-BR',{timeStyle: 'long', dateStyle: 'short'}).format(Date.now());
+
     const dadosUsuarioCompleto = {
         ...dadosUsuario,
         // (... operador rest)
@@ -60,3 +82,79 @@ function OrganizarDados(dadosUsuario, valorImc, classificacaoImc) {
     return dadosUsuarioCompleto;
 }
 
+function CadastrarUsuario(usuario) {
+
+    //cria um array vazio para armazenar os valores do usuário;
+    let listaUsuario = [];
+
+    //verifica se dentro do localStorege eu tenho as informações do usuário;
+    if(localStorage.getItem('usuariosCadastrados')) {
+        //se sim, eu guardo as informações dentro do array;
+        //parse => de JSON para object;
+        listaUsuario = JSON.parse(localStorage.getItem('usuariosCadastrados'));
+    }
+
+    //cadastrar usuário dentro do array;
+    listaUsuario.push(usuario);
+
+    //caso contrário, eu crio um novo item no localStorege;
+    //stringify => object para JSON;
+    localStorage.setItem('usuariosCadastrados', JSON.stringify(listaUsuario));
+}
+
+function CarregarUsuarios() {
+    
+    let listaUsuario = [];
+
+    if(localStorage.getItem('usuariosCadastrados')) {
+        listaUsuario = JSON.parse(localStorage.getItem('usuariosCadastrados'));
+    }
+
+    if(listaUsuario.length == 0) {
+        let tabela = document.getElementById('corpo-tabela');
+
+        tabela.innerHTML = `
+            <tr class='linha-mensagem'>
+                <td colspan='6'> Nenhum usuário cadastrado. </td>
+            </tr>
+        `
+    }
+    else {
+        MontarTabela(listaUsuario);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => CarregarUsuarios());
+
+function MontarTabela(listaDeCadastrados) {
+    let tabela = document.getElementById('corpo-tabela');
+
+    let template = '';
+
+    listaDeCadastrados.forEach(pessoa => {
+        template += `
+            <tr>
+                <td data-cell='nome'> ${pessoa.nome} </td>            
+                <td data-cell='altura'> ${pessoa.altura} </td>            
+                <td data-cell='peso'> ${pessoa.peso} </td>            
+                <td data-cell='imc'> ${pessoa.imc} </td>            
+                <td data-cell='classificacao'> ${pessoa.classificacaoImc} </td>            
+                <td data-cell='dataCadastro'> ${pessoa.dataCadastro} </td>            
+            </tr>
+        `
+    });
+
+    tabela.innerHTML = template;
+}
+
+function DeletarRegistros () {
+    
+    localStorage.clear();
+
+    let tabela = document.getElementById('corpo-tabela');
+    tabela.innerHTML = `
+        <tr class='linha-mensagem'>
+            <td colspan='6'> Nenhum usuário cadastrado. </td>
+        </tr>
+    `;
+} 
